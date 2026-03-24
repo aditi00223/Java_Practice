a0b4981eb4bc69a91571c503b72d67bd735420e1
Aditi Sharma <aditisharma032007@gmail.com>
2026-03-24 09:48:42 +0530

    Char to string

---
+++ CharString.java
@@ -0,0 +1,9 @@
+public class CharString {
+    public static void main(String[] args) {
+        char[] s = { 'H', 'e', 'l', 'l', 'o' }; 
+        for(char c : s) {
+            System.out.print(c+"");
+        }
+    }
+    
+}
