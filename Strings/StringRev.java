509d27d7eeb5287bfb0def8fd0c448413bc6ffe7
Aditi Sharma <aditisharma032007@gmail.com>
2026-03-28 21:57:20 +0530

    Strings 1st program

---
+++ Strings/StringRev.java
@@ -0,0 +1,14 @@
+package Strings;
+
+public class StringRev {
+    
+    public static void main(String[] args) {
+        String str = "hello";
+
+        String reversed = new StringBuilder(str).reverse().toString();
+
+        System.out.println(reversed);
+    }
+}
+    
+
