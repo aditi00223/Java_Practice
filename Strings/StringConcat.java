0749ef84f4bb62b3515eb9d57b66287b1bc5b9a8
Aditi Sharma <aditisharma032007@gmail.com>
2026-03-28 22:40:56 +0530

    Concatenate Two string

---
+++ Strings/StringConcat.java
@@ -0,0 +1,16 @@
+package Strings;
+
+public class StringConcat {
+    
+    public static void main(String[] args) {
+
+        String s1 = "Hello ";
+        String s2 = "World";
+
+        String result = s1.concat(s2);
+
+        System.out.println(result);
+    }
+}
+    
+
---
+++ Strings/StringConcat1.java
@@ -0,0 +1,15 @@
+package Strings;
+
+public class StringConcat1 {
+    
+    public static void main(String[] args) {
+
+        String s1 = "Hello ";
+        String s2 = "World";
+
+        String result = s1 + s2;
+
+        System.out.println(result);
+    }
+    
+}
