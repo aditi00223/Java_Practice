41dd62e620ec80cc2ab3054dfccbcac3e05bfaf3
Aditi Sharma <aditisharma032007@gmail.com>
2026-03-28 22:10:46 +0530

    Checking Strings are Equal or not

---
+++ Strings/StringEqual.java
@@ -0,0 +1,17 @@
+package Strings;
+
+public class StringEqual {
+    
+    public static void main(String[] args) {
+        String s1 = "hello";
+        String s2 = "hello";
+
+        if (s1.equals(s2)) {
+            System.out.println("Strings are equal");
+        } else {
+            System.out.println("Strings are not equal");
+        }
+    }
+}
+    
+
